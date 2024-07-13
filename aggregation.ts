4feb6d8848a9a359f26d27078faecca856d8db8f
abdul <mohammadabdulrehman739@gmail.com>
2024-07-13 13:32:19 +0530

    added project stage

--- aggregation.ts
+++ aggregation.ts
@@ -4,28 +4,36 @@ type DeepPartial<T> = T extends object
     }
   : T;
 
-type FilterStringKeys<T> = {
-  [K in keyof T]: K extends string ? K : never;
-}[keyof T];
+type ZeroOrOne<T> = {
+  [k in keyof T]: T[k] extends object ? ZeroOrOne<T[k]> : 0 | 1;
+};
 
-interface Test {
-  name: string;
-  email: string;
-}
+type ProjectOutput<T, R extends ZeroOrOne<T>> = {
+  [K in keyof T as R[K] extends 1 | object ? K : never]: T[K] extends object
+    ? ProjectOutput<T[K], R[K] & ZeroOrOne<T[K]>>
+    : T[K];
+};
 
 // the match stage should return what ever the collection passed to it
 type MatchStage<Coll> = Record<"$match", DeepPartial<Coll>>;
-type ProjectStage<Coll> = Record<"$project", Record<keyof Coll, 0 | 1>>;
+type ProjectStage<Coll> = Record<"$project", ZeroOrOne<Coll>>;
 
 type Pipeline<Coll> = MatchStage<Coll> | ProjectStage<Coll>;
 
+interface database {
+  users: ITest;
+}
 interface ITest {
   name: string;
   email: string;
   verified: boolean;
+  details: {
+    city: "hyderabad" | "delhi";
+    street: string;
+  };
 }
 
-class BaseAggregate<T> {
+export class BaseAggregate<DB extends object, T> {
   private pipeline: Pipeline<T>[] = [];
   constructor(initialPipeline?: typeof this.pipeline) {
     if (initialPipeline) {
@@ -34,18 +42,15 @@ class BaseAggregate<T> {
   }
 
   match(stageInput: DeepPartial<T>) {
-    return new BaseAggregate<T>([...this.pipeline, { $match: stageInput }]);
+    return new BaseAggregate<DB, T>([...this.pipeline, { $match: stageInput }]);
   }
 
-  projectActual<R extends Record<keyof T, 0 | 1>>(stageInput: R) {
-    type Output = {
-      [K in keyof T as R[K] extends 1 ? K : never]: T[K];
-    };
-
-    return new BaseAggregate<Output>([
+  project<R extends ZeroOrOne<T>>(stageInput: R) {
+    const newPipeline: Pipeline<T>[] = [
       ...this.pipeline,
       { $project: stageInput },
-    ]);
+    ];
+    return new BaseAggregate<DB, ProjectOutput<T, R>>(newPipeline);
   }
 
   logPipeline() {
@@ -53,13 +58,16 @@ class BaseAggregate<T> {
   }
 }
 
-const agg = new BaseAggregate<Test>()
+const agg = new BaseAggregate<database, ITest>()
   .match({ name: "abdul" })
   .match({ email: "sir abdul" })
-  // .project({ email: "abd@mail.com", newProperty: "something", something: true })
   .match({ email: "sir" })
-  // .project({});
-  .projectActual({ email: 0, name: 1 })
-  .match({ name: "hai" });
+  .project({
+    email: 0,
+    name: 1,
+    details: { city: 1, street: 1 },
+    verified: 1,
+  })
+  .match({ details: { street: "nice", city: "delhi" } });
 
 agg.logPipeline();
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { Db, MongoClient } from "mongodb";
-import { Collection } from "mongodb";
+import { Db, MongoClient, ObjectId } from "mongodb";
+import { BaseAggregate } from "./aggregation.js";
 
+type Generated<T = ObjectId> = T | undefined;
 export type DeepPartial<T> = T extends object
   ? {
       [P in keyof T]?: DeepPartial<T[P]>;
@@ -19,7 +20,7 @@ class BaseFind<Coll> {
   }
 }
 
-class BaseCollection<Coll> {
+class BaseCollection<DB extends object, Coll> {
   private db: Db;
   private name: string;
   constructor(db: Db, name: string) {
@@ -27,13 +28,16 @@ class BaseCollection<Coll> {
     this.name = name;
   }
 
-  // async find<K extends Coll>(key: DeepPartial<K>): Promise<Coll | null> {}
   find<K extends Coll>(filterCondition: DeepPartial<K>): BaseFind<Coll> {
     return new BaseFind<Coll>(filterCondition);
   }
+
+  aggregate(): BaseAggregate<DB, Coll> {
+    return new BaseAggregate<DB, Coll>([]);
+  }
 }
 
-class BetterGoose<DB> {
+class BetterGoose<DB extends object> {
   private static URI: string;
   private static Database: string;
   private static db: Db | null;
@@ -43,11 +47,11 @@ class BetterGoose<DB> {
     BetterGoose.Database = database;
   }
 
-  collection<K extends keyof DB>(name: K & string): BaseCollection<DB[K]> {
+  collection<K extends keyof DB>(name: K & string): BaseCollection<DB, DB[K]> {
     if (!BetterGoose.db) {
       throw new Error("No db connection");
     }
-    return new BaseCollection<DB[K]>(BetterGoose.db, name);
+    return new BaseCollection<DB, DB[K]>(BetterGoose.db, name);
   }
 
   public connect = async (): Promise<void> => {
@@ -67,14 +71,42 @@ class BetterGoose<DB> {
 
 export default BetterGoose;
 
-// class BaseFind<Coll> {
-//   private filter: DeepPartial<Coll>;
+// ------------------------------------------------------
 
-//   constructor(filter: DeepPartial<Coll>) {
-//     this.filter = filter;
-//   }
+export interface IDb {
+  users: {
+    _id: Generated;
+    name: string;
+    email: string;
+    verified: boolean;
+    address: {
+      city: "Hyderabad" | "bengaluru";
+      street: string;
+    };
+  };
+  orders: {
+    price: number;
+    created_at: Date;
+  };
+}
+const URI = "mogodb+srv://some";
+const Database = "Dev";
+const db = new BetterGoose<IDb>(URI, Database);
 
-//   someOther<T extends typeof this.filter>(key: T) {
-//     return key;
-//   }
-// }
+const main = async () => {
+  await db.connect();
+
+  const a = db.collection("users").find({ address: { city: "Hyderabad" } });
+  const b = db
+    .collection("users")
+    .aggregate()
+    .match({ email: "somemail.com" })
+    .project({
+      _id: 1,
+      email: 0,
+      name: 1,
+      verified: 0,
+      address: { city: 0, street: 1 },
+    })
+    .match({ address: { street: "nice street" } });
+};
